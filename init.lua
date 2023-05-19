75db02cdb2fd92f06a6166d322ef00bacf351f06
jixuecao <2386900869@qq.com>
2023-05-19 10:54:07 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,23 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
 
 
--- local opt = vim.opt
--- local g = vim.g
--- local config = require("core.utils").load_config()
+local opt = vim.opt
+local g = vim.g
+local config = require("core.utils").load_config()
 --
 -- -------------------------------------- globals -----------------------------------------
 -- g.python3_host_prog='~/anaconda3/bin/python3'
 --
 -- -------------------------------------- options ------------------------------------------
 -- --opt.softtabstop = 2
+-- 禁止生成交换文件
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
