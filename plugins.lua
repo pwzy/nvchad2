6d8a8b3c5e06de294ae2cb41cf437caff76ae8dd
jixuecao <2386900869@qq.com>
2023-05-25 11:45:26 +0800

    Update plugins.lua

--- plugins.lua
+++ plugins.lua
@@ -103,17 +103,17 @@ local plugins = {
     "mg979/vim-visual-multi",
     event = "BufEnter",
   },
-
+  
   -- 更改成对符号
   {
-    "tpope/vim-surround",
-    event = "BufEnter",
-  },
-
-  -- 使surround插件操作能重复的插件 
-  {
-    "tpope/vim-repeat",
-    event = "BufEnter",
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   },
 
   -- 利用f快速移动
