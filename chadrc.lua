7b1a4811fe915e039a191a55c15d40b187ded56f
jixuecao <2386900869@qq.com>
2023-05-26 17:33:04 +0800

    Update chadrc.lua

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
+  theme = "rosepine",
+  theme_toggle = { "rosepine", "one_light" },
   
   -- nvdash (dashboard)
   nvdash = {
